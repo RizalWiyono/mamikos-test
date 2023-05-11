fcfe76cf8e3ee1cc08577c403c6df1b5c4a098d5
Muhammad Rizal Wiyono <muhammadrizalwiyono@gmail.com>
2023-05-11 23:26:18 +0700

    chore: add feature search, fetch data, and component

---
+++ src/components/SearchFilter/SearchFilter.vue
@@ -0,0 +1,49 @@
+<!-- eslint-disable prettier/prettier -->
+<template>
+  <input 
+    ref="inputSearch" 
+    v-model="searchTerm" 
+    type="search" 
+    @input="handleSearchInputChange" 
+    :placeholder="placeholder"
+  />
+</template>
+<!-- eslint-disable prettier/prettier -->
+
+<script>
+export default {
+  name: "SearchFilter",
+  props: {
+    placeholder: {
+      type: String,
+      required: true,
+    },
+    searchAction: {
+      type: Function,
+      required: true,
+    },
+  },
+  data() {
+      return {
+        searchTerm: "",
+      }
+  },
+  methods: {
+    handleSearchInputChange() {
+      this.searchAction(this.searchTerm)
+		},
+  }
+};
+</script>
+<!-- eslint-disable prettier/prettier -->
+
+<style scoped>
+  input {
+    padding: 10px;
+    width: 300px;
+    border: 0;
+    border-radius: 8px;
+    cursor: pointer;
+    border: 1px solid #d3d3d3;
+  }
+</style>
---
+++ src/components/Table/Table.vue
@@ -0,0 +1,77 @@
+<!-- eslint-disable prettier/prettier -->
+<template>
+    <table>
+        <thead>
+            <tr>
+                <th v-for="header in header" :key="header">
+                    {{ header  }}
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(item, index) in body" :key="index">
+                <td style="width: 45px; text-align: center;">
+                    <img :src="item.photo" :alt="item.name" >
+                </td>
+                <td>
+                    {{ 
+                        item.name    
+                    }}
+                </td>
+                <td>
+                    {{ 
+                        item.age    
+                    }}
+                </td>
+                <td>
+                    {{ 
+                        item.job    
+                    }}
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+<!-- eslint-disable prettier/prettier -->
+
+<script>
+export default {
+    name: "TableData",
+    props: {
+        header: Array,
+        body: Array,
+    },
+};
+</script>
+<!-- eslint-disable prettier/prettier -->
+
+<style scoped>
+    table {
+        border-radius: 8px;
+        width: 100%;
+        background-color: #FFFFFF;
+        border-collapse: collapse;
+        border-width: 1px;
+        border-color: #D3D3D3;
+        border-style: solid;
+        color: #000000;
+    }
+
+    table td, table th {
+        border-width: 1px;
+        border-color: #D3D3D3;
+        border-style: solid;
+        padding: 15px;
+    }
+
+    table thead {
+        background-color: #FFF;
+    }
+
+    tbody tr td img {
+        width: 40px;
+        height: 40px;
+        object-fit: cover;
+        border-radius: 4px;
+    }
+</style>
